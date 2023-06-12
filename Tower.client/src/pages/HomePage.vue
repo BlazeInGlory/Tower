faea21301ce41ac36ad4af06571956de0299362d
Jibbles <jibbles208@gmail.com>
2023-06-11 19:41:04 -0600

    filterdone

--- Tower.client/src/pages/HomePage.vue
+++ Tower.client/src/pages/HomePage.vue
@@ -1,4 +1,15 @@
 <template>
+  <section class="row justify-content-center col-12">
+  <div class="">
+    <div class="justify-content-around my-3 bg-info rounded p-3">
+      <button @click="filterBy=''" class="btn btn-outline-light w-25 mx-2">All</button>
+      <button @click="filterBy='concert'" class="btn btn-outline-light w-25 mx-2">Concert</button>
+      <button @click="filterBy='convention'" class="btn btn-outline-light w-25 mx-2">Convention</button>
+      <button @click="filterBy='sport'" class="btn btn-outline-light w-25 mx-2">Sports</button>
+      <button @click="filterBy='digital'" class="btn btn-outline-light w-25 mx-2">Digitals</button>
+    </div>
+  </div>
+</section>
   <div class="container-fluid">
     <section class="row">
       <div class="col-md-4" v-for="e in events" :key="e.id">
@@ -18,6 +29,7 @@ import { computed } from '@vue/reactivity'
 import { AppState } from '../AppState'
 import EventCard from '../components/EventCard.vue'
 export default {
+  name: "Home",
   setup() {
     const filterBy = ref('')
       async function getEvents() {
@@ -31,10 +43,16 @@ export default {
   onMounted(() => {
     getEvents()
   })
-    return {
-      filterBy,
-        events: computed(() => AppState.events)
-    }
+  return {
+    filterBy,
+      events: computed(() => {
+        if(filterBy.value == "") {
+          return AppState.events
+        }else {
+          return AppState.events.filter(e=>filterBy.value ? e.type == filterBy.value : true)
+      }
+    })
+  }
   },
   components: { EventCard }
 }
